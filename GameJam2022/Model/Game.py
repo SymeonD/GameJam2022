acc6aca769df77c3be374edd1baaca1e1c220842
SymeonD <symeon.dealmeida@gmail.com>
2022-02-07 17:11:01 +0100

    Character movement

--- GameJam2022/Model/Game.py
+++ GameJam2022/Model/Game.py
@@ -17,22 +17,42 @@ class Game:
         map_data = pyscroll.data.TiledMapData(tmx_data);
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size());
 
-        self.player = Player(30, 40, "Player1")
+        player_position = tmx_data.get_object_by_name('player')
+        self.player = Player(player_position.x, player_position.y, "Player1")
 
-        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=3)
         self.group.add(self.player)
 
-    def run(self):
+    def handle_input(self):
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_UP]:
+            self.player.move_up()
+            self.player.change_animation('up')
+        elif pressed[pygame.K_DOWN]:
+            self.player.move_down()
+            self.player.change_animation('down')
+        elif pressed[pygame.K_RIGHT]:
+            self.player.move_right()
+            self.player.change_animation('right')
+        elif pressed[pygame.K_LEFT]:
+            self.player.move_left()
+            self.player.change_animation('left')
 
+    def run(self):
+        clock = pygame.time.Clock()
         running = True;
 
         while running:
+            self.handle_input()
             self.group.update()
+            self.group.center(self.player.rect)
             self.group.draw(self.screen)
             pygame.display.flip()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False;
+
+            clock.tick(60)
 
         pygame.quit()
--- GameJam2022/Model/Player.py
+++ GameJam2022/Model/Player.py
@@ -5,20 +5,39 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, name):
         super().__init__()
         self.sprite_sheet = pygame.image.load('../Ressources/player.png')
-        self.image = self.getImage(0,0)
+        self.image = self.get_image(0,0)
         self.image.set_colorkey([0, 0, 0])
         self.rect = self.image.get_rect()
         self.position = [x, y]
+        self.images = {
+            'down': self.get_image(0, 0),
+            'up': self.get_image(0, 96),
+            'right': self.get_image(0, 64),
+            'left': self.get_image(0, 32)
+        }
+        self.speed = 3
 
         self.name = name;
         self.health = 200;
         self.skin = 1;
         self.inventory = [];
 
+    def change_animation(self, name):
+        self.image.set_colorkey((0,0,0))
+        self.image = self.images[name]
+
+    def move_right(self): self.position[0] += self.speed
+
+    def move_left(self): self.position[0] -= self.speed
+
+    def move_up(self): self.position[1] -= self.speed
+
+    def move_down(self): self.position[1] += self.speed
+
     def update(self):
         self.rect.topleft = self.position
 
-    def getImage(self, x, y):
+    def get_image(self, x, y):
         image = pygame.Surface([32, 32])
         image.blit(self.sprite_sheet, (0, 0), (x, y, 32, 32))
         return image
