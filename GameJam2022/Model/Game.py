f59b007afdaf93ed9a4feae7f5fbf3772d4e15e4
Wibaillie Valentin <84399994+Valentin-IUT2@users.noreply.github.com>
2022-02-07 18:45:15 +0100

    Ajout du menu pause en jeu

--- GameJam2022/Model/Game.py
+++ GameJam2022/Model/Game.py
@@ -5,19 +5,20 @@ import pyscroll
 import Player
 import NPC_Werewolf
 import NPC
+import bouton
 
 
 class Game:
 
     def __init__(self):
 
-        self.screen = pygame.display.set_mode((1024, 768));
-        pygame.display.set_caption("WereWolf");
+        self.screen = pygame.display.set_mode((1024, 768))
+        pygame.display.set_caption("WereWolf")
 
         # Charger la carte du jeu
-        tmx_data = pytmx.util_pygame.load_pygame("../Ressources/MapTest.tmx");
-        map_data = pyscroll.data.TiledMapData(tmx_data);
-        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size());
+        tmx_data = pytmx.util_pygame.load_pygame("../Ressources/MapTest.tmx")
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
 
         #Spawn player
         player_position = tmx_data.get_object_by_name('player')
@@ -58,10 +59,12 @@ class Game:
         elif pressed[pygame.K_LEFT]:
             self.player.move_left()
             self.player.change_animation('left')
+        elif pressed[pygame.K_p]:
+            self.paused()
 
     def run(self):
         clock = pygame.time.Clock()
-        running = True;
+        running = True
 
         while running:
             self.handle_input()
@@ -72,8 +75,39 @@ class Game:
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False;
+                    running = False
 
             clock.tick(60)
 
+        pygame.quit()
+
+    def paused(self):
+        run = True
+        #chargement de l'image de fond
+        background_img = pygame.image.load('../Ressources/bg.jpg').convert_alpha()
+
+        #chargement des images pour boutons
+        resume_img = pygame.image.load('../Ressources/start_btn.png').convert_alpha()
+        leave_img = pygame.image.load('../Ressources/exit_btn.png').convert_alpha()
+
+        #creer les boutons
+        resume_button = bouton.Button(100, 200, resume_img, 0.5)
+        leave_button = bouton.Button(600, 200, leave_img, 0.5)
+
+        while run:
+
+            self.screen.blit(background_img, (0, 0))
+
+            if resume_button.draw(self.screen):
+                self.run()
+            if leave_button.draw(self.screen):
+                run = False
+
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+            pygame.display.update()
+            
         pygame.quit()
