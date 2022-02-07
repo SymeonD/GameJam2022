f2a3ba5a38ec8b09ea3b2bd79379671764a7dfe2
Wibaillie Valentin <84399994+Valentin-IUT2@users.noreply.github.com>
2022-02-07 15:12:52 +0100

    Add files via upload

---
+++ GameJam2022/Model/bouton.py
@@ -0,0 +1,31 @@
+import pygame
+
+#classe bouton
+class Button():
+	def __init__(self, x, y, image, scale):
+		width = image.get_width()
+		height = image.get_height()
+		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+		self.rect = self.image.get_rect()
+		self.rect.topleft = (x, y)
+		self.clicked = False
+
+	def draw(self, surface):
+		action = False
+
+		#retourne la position de la souris
+		pos = pygame.mouse.get_pos()
+
+		#verifie si la souris passe au dessus du bouton et si la condition clicked est vrai
+		if self.rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+				self.clicked = True
+				action = True
+
+		if pygame.mouse.get_pressed()[0] == 0:
+			self.clicked = False
+
+		#dessine un bouton a l'ecran
+		surface.blit(self.image, (self.rect.x, self.rect.y))
+
+		return action
