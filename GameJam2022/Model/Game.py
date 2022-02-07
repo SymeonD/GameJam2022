5fcb26009af653eafd3f4a4230b01fd231b7ff27
SymeonD <symeon.dealmeida@gmail.com>
2022-02-07 16:11:12 +0100

    Ajout player

--- GameJam2022/Model/Game.py
+++ GameJam2022/Model/Game.py
@@ -2,6 +2,9 @@ import pygame
 import pytmx
 import pyscroll
 
+from GameJam2022.Model.Player import Player
+
+
 class Game:
 
     def __init__(self):
@@ -14,13 +17,17 @@ class Game:
         map_data = pyscroll.data.TiledMapData(tmx_data);
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size());
 
+        self.player = Player("Player1", 30, 40)
+
         self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
+        self.group.add(self.player)
 
     def run(self):
 
         running = True;
 
         while running:
+            self.group.update()
             self.group.draw(self.screen)
             pygame.display.flip()
 
