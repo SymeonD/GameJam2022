42fd768779864df5d99b197c0e547605bb72c2b1
Wibaillie Valentin <84399994+Valentin-IUT2@users.noreply.github.com>
2022-02-09 11:49:45 +0100

    Ressources items + restructuration item.py

--- Model/item.py
+++ Model/item.py
@@ -1,11 +1,20 @@
 import pygame
 
-items = [pygame.Surface((50,50),pygame.SRCALPHA)]
+#items = [pygame.Surface((50,50),pygame.SRCALPHA)]
 
 class Item:
-    def __init__(self,id):
-        self.id = id
-        self.surface = items[id]
+    def __init__(self,id,name,roll,image):
+        self.id = id #id de l'item dans la liste
+        self.name = name #nom de l'item
+        self.roll = roll #chance de drop
+        self.image = image #image de l'item
+        #self.surface = items[id]
     
     def resize(self,size):
         return pygame.transform.scale(self.surface,(size,size))
+
+#créer la liste d'item
+itemList = []
+
+#pour ajouter un item à notre liste
+list.append(Item(0,'Potion de Vie',5, (pygame.image.load('Ressources/item/tile144.png'))))
