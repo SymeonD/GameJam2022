8bdd382215027e6a51f668784ffecb4ea6329620
Chalekh Zineddine <zineddine@MacBook-Air-de-Chalekh.local>
2022-02-09 16:20:50 +0100

    changement de map

---
+++ Ressources/tsx/insideH.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="insideH" tilewidth="16" tileheight="16" tilecount="725" columns="25">
+ <image source="../element/inside.png" trans="282828" width="410" height="466"/>
+</tileset>
