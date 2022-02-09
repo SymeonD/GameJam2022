0cea5d580c697ac89eebd24a619ddbd4ae7b8916
zineddineClk <zine.clkh@gmail.com>
2022-02-09 23:27:51 +0100

    portals + superposition calques

--- Model/Game.py
+++ Model/Game.py
@@ -6,6 +6,7 @@ import pyscroll
 import sys
 import math
 from pygame import mixer
+from Model.Map import MapManager
 
 from Model.bouton import Button
 from Model.Player import Player
@@ -24,46 +25,15 @@ class Game:
         pygame.display.set_caption('WereWolf')
 
         #map de base
-        self.map = "town"
-
-        #chargement de la map(tmx)
-        tmx_data = pytmx.util_pygame.load_pygame("Ressources/town_day.tmx")
-        map_data = pyscroll.data.TiledMapData(tmx_data)
-        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
-        #map_layer.zoom = 2
+        self.map = "town_day"
 
         #chargement du joueur
-        player_position = tmx_data.get_object_by_name('player')
-        self.player = Player(player_position.x, player_position.y, self.screen)
+        self.player = Player(0, 0, self.screen)
+
+        self.map_manager = MapManager(self.screen, self.player)
 
             #définir le logo du jeu
         pygame.display.set_icon(self.player.get())
-
-        #chargement des PNJ
-        self.werewolf_positions = []
-        self.werewolfs = []
-
-        for i in range(4):
-            name = 'LG' + str(i)
-            werewolf_position = tmx_data.get_object_by_name(name)
-            self.werewolf_positions.append(werewolf_position)
-
-        for werewolf_spawn in self.werewolf_positions:
-            werewolf = NPC_Werewolf(werewolf_spawn.x, werewolf_spawn.y, 'Werewolf', self.screen, random.randint(1,5))
-            self.werewolfs.append(werewolf)
-
-        # Les collisions
-        self.walls = []
-
-        for obj in tmx_data.objects:
-            if obj.type == "collision":
-                self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
-
-        #grouper les calques
-        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=0)
-        self.group.add(self.player)
-        self.werewolf_group = pygame.sprite.Group()
-        self.werewolf_group.add(self.werewolfs)
 
         #Création d'une clock pour les FPS
         self.clock = pygame.time.Clock()
@@ -89,7 +59,7 @@ class Game:
     def run(self):
         running = True
 
-        mixer.music.play() #lecture de la musique
+        #mixer.music.play() #lecture de la musique
 
         while running:
             #Limiter à 60 fps
@@ -97,20 +67,15 @@ class Game:
             #gestion des cycles jour/nui
             self.switch_cycle()
 
-            #centrer la caméra sur le joueur
-            self.group.center(self.player.rect)
-            #dessiner les calques
-            self.group.draw(self.screen)
+            # Dessin de la map + centrage
+            self.map_manager.draw()
+
             #sauvegarde localisation joueur
             self.player.save_location()
             #gestion des touches
             self.handle_input()
-            #gestion des interactions PNJ
-            self.handle_npc_interaction()
-            #maj des loups
-            self.move_werewolfs()
 
-            self.refresh()
+            self.update()
 
             #event handler
             for event in pygame.event.get():
@@ -121,12 +86,6 @@ class Game:
                 if event.type == pygame.MOUSEBUTTONUP:
                     pos = pygame.mouse.get_pos()
 
-                    # Attaque
-                    clicked_sprites = [s for s in self.werewolf_group if s.rect.collidepoint(pos)]
-                    for werewolf in clicked_sprites:
-                        attackSound = mixer.Sound("Ressources/sounds/player_attack.ogg")
-                        attackSound.play()
-                        self.player.attack(werewolf)
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     pos = pygame.mouse.get_pos()
@@ -151,14 +110,9 @@ class Game:
         if keys[pygame.K_p]:
             self.pause()
 
-    def handle_npc_interaction(self):
-        for werewolf in self.werewolf_group:
-            playerDistance = math.hypot(werewolf.position[0] - self.player.position[0],
-                                             werewolf.position[1] - self.player.position[1])
-            if playerDistance < 50 and werewolf.state == "NPC":
-                werewolf.startDialog()
 
     def switch_cycle(self):
+        
 
         #Récupère les secondes
         seconds = (pygame.time.get_ticks()-self.start_ticks)//1000
@@ -174,51 +128,14 @@ class Game:
                     self.cycleMoon = 1
                     #Add special effects (super werewolves...)
 
-                # Change to werewolf
-                for werewolf in self.werewolf_group:
-                    werewolf.transform(self.cycleMoon)
 
-            else:
-                self.cycleState = "jour"
+    def update(self):
 
-                for werewolf in self.werewolf_group:
-                    werewolf.transform(6)
-
-    def move_werewolfs(self):
-        if self.cycleState == "nuit":
-            for werewolf in self.werewolf_group:
-                werewolf.move_npc(self.player)
-
-    def refresh(self):
-        #Update entities
-
-        self.group.update()
-        self.werewolf_group.update()
-
-        #Blit entities
-        self.screen.blit(self.player.image, self.player.rect)
-        for werewolf in self.werewolf_group:
-            self.screen.blit(werewolf.image, werewolf.rect)
-
-        #afficher l'inventaire
+        self.map_manager.update()
         Inventory.update(self.player.inventory, self.screen, self.player.inventory)
 
-        # Vérification des collisions
-        for sprite in self.group.sprites():
-            if sprite not in self.werewolfs:
-                if sprite.feet.collidelist(self.walls) > -1:
-                    sprite.move_back()
-
-                #Degats sur le joueur
-                for werewolf in self.werewolf_group:
-                    if self.player.rect.colliderect(werewolf.rect) and werewolf.state == "WW":
-                        self.player.take_damage(werewolf.damage, werewolf.position[0], werewolf.position[1])
-                        damageSound = mixer.Sound("Ressources/sounds/player_hit.ogg")
-                        damageSound.play()
-
-        # actualisation
-        # pygame.display.flip()
         pygame.display.update()
+
 
     def pause(self):
         runPause = True
---
+++ Model/Map.py
@@ -0,0 +1,125 @@
+from dataclasses import dataclass
+from re import S
+import pygame, pytmx, pyscroll
+
+@dataclass
+class Portal:
+    from_world: str
+    origin_point: str
+    target_world: str
+    teleport_point: str
+
+@dataclass
+class Map:
+    name: str
+    walls: list[pygame.Rect]
+    group: pyscroll.PyscrollGroup
+    tmx_data: pytmx.TiledMap
+    portals : list[Portal]
+
+class MapManager:
+
+    def __init__(self, screen, player):
+        self.maps = dict() # town_day -> Map("town_day", walls, group)
+        self.screen = screen
+        self.player = player
+        self.current_map = "town_day"
+
+        #chargement des maps
+        self.register_map("town_day", portals=[
+            Portal(from_world="town_day", origin_point="enter_house", target_world="medium_house", teleport_point="spawn_house")
+        ])
+        self.register_map("medium_house", portals=[
+            Portal(from_world="medium_house", origin_point="exit_house", target_world="town_day", teleport_point="spawn_exit_house")
+        ])
+
+        self.teleport_player("player")
+
+    def register_map(self, name, portals=[]) :
+         # Charger la map
+        tmx_data = pytmx.util_pygame.load_pygame(f"Ressources/{name}.tmx")
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        #map_layer.zoom = 2
+
+        # Les collisions
+        walls = []
+        for obj in tmx_data.objects:
+            if obj.type == "collision":
+                print("test")
+                walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+
+        '''
+        # Dessiner les différents calques
+            #gestion de la superposition du joueur avec le décor en fonction des calques
+        if name == "town_day":
+            dl = 6
+        elif name == "medium_house":
+            dl = 1
+
+            -> PROBLEME RESOLU : 8 claques max avec le top
+        '''
+
+        group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=6)
+        group.add(self.player)
+
+        # Creer un objet Map
+        map = Map(name, walls, group, tmx_data, portals)
+        self.maps[name] = map
+
+
+    '''
+    - Getters
+    '''
+    def get_map(self):
+        return self.maps[self.current_map]
+
+    def get_group(self):
+        return self.get_map().group
+
+    def get_walls(self):
+        return self.get_map().walls
+
+    def get_object(self, name):
+        return self.get_map().tmx_data.get_object_by_name(name)
+
+    '''
+    - Dessin du groupe + centrage sur le joueur
+    '''
+    def draw(self):
+        self.get_group().center(self.player.rect.center)
+        self.get_group().draw(self.screen)
+        
+    '''
+    - Update de la map
+    '''
+    def update(self):
+        self.get_group().update()
+        self.check_collisions()
+
+    '''
+    - Téléportation du joueur
+    '''
+    def teleport_player(self, name):
+        point = self.get_object(name)
+        self.player.position[0] = point.x
+        self.player.position[1] = point.y
+        self.player.save_location()
+
+    '''
+    - Verification des collisions
+    '''
+    def check_collisions(self):
+        for portal in self.get_map().portals:
+            if portal.from_world == self.current_map:
+                point = self.get_object(portal.origin_point)
+                rect = pygame.Rect(point.x, point.y, point.width, point.height)
+
+                if self.player.feet.colliderect(rect):
+                    copy_portal = portal
+                    self.current_map = portal.target_world
+                    self.teleport_player(copy_portal.teleport_point)
+
+        for sprite in self.get_group().sprites():
+            if sprite.feet.collidelist(self.get_walls()) > -1:
+                sprite.move_back()
